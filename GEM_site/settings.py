41e19b483738e5eafa2d7a4646ca47a6dbd3bdf3
Yamil Elías <yamileliassoto@gmail.com>
2016-03-21 00:33:23 -0700

    Admin override
    Made changes to settings.py and adding a base.css file to override admin
    stylesheet.

--- GEM_site/settings.py
+++ GEM_site/settings.py
@@ -40,13 +40,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gem.apps.GemConfig', # Goes first to override /admin/css/base.css
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gem.apps.GemConfig',
     'bootstrap3',
 ]
 
