161de7d8b8d04507551c73e251d05f3bb25fe4b8
Yamil Elías <yamileliassoto@gmail.com>
2016-03-27 01:09:57 -0700

    Added runserver compatibility

--- gem/views.py
+++ gem/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from models import GrupoEstudiantil, Evento, Asistencia, Usuario
+from gem.models import GrupoEstudiantil, Evento, Asistencia, Usuario
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.views import generic
